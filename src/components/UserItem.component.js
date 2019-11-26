24f4ab71d472e73b3143778470231e332a65e73d
AlenaNik <alena.nik.digital@gmail.com>
2019-11-26 21:47:42 +0100

    user data

---
+++ src/components/UserItem.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './UserItem.css';
+
+
+const UserItem = () => {
+    return <h1> New Place </h1>
+}
+
+
+export default UserItem;
--- src/pages/Users.page.js
+++ src/pages/Users.page.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import UserList from '../components/UsersList.component';
 
 const Users = () => {
+    const USERS = [{
+        id: 'user1',
+        name: 'Batosha',
+        image: 'https://images.unsplash.com/photo-1548681528-6a5c45b66b42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80',
+        places: 11
+    }];
+
     return (
-        <h1>I'm a user</h1>
+       <UserList items={USERS}/>
     )
 };
 
