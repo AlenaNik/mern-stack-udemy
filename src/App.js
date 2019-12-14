ab336f791a1e59a1786c839a77281fd07b77860b
AlenaNik <alena.nik.digital@gmail.com>
2019-12-14 19:17:45 +0100

    component drawer

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './pages/Users.page';
 import NewPlace from "./pages/NewPlace.page";
+import MainNavitation from "./components/Navigation/MainNavigation";
 
 const App = () => {
     return <Router>
+            <MainNavitation/>
+            <main>
             <Switch>
                     <Route exact path="/places/add-new">
                         <NewPlace />
@@ -14,6 +17,7 @@ const App = () => {
                     </Route>
                 <Redirect to="/" />
             </Switch>
+            </main>
            </Router>
 };
 
--- src/components/Navigation/MainHeader.js
+++ src/components/Navigation/MainHeader.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './MainHeader.css';
 
 const MainHeader = props => {
-    return <header className="main-header"
+    return <header className="main-header">
+        {props.children}
     </header>;
-}
+};
+
 export default MainHeader;
